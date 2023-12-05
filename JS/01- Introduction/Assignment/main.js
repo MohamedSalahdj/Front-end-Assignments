560264747dfe3056e5fd5f5bfbc6fd7d5e860587
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-12-05 20:46:07 +0200

    modified in assignment-01

--- JS/01- Introduction/Assignment/main.js
+++ JS/01- Introduction/Assignment/main.js
@@ -1,3 +1,35 @@
 
 //? assignment-02
 document.write("<h1 style='color: blue;font-size: 80px;font-weight: bold;text-align: center;font-family: Arial'> Elzero</h1>")
+
+
+//? assignment-03
+console.log("%cElzero %cWeb %cSchool","color:red;font-size:40px","color:green;font-size:40px;font-weight:bold;", "color:blue;font-size:40px" )
+
+//? assignment-04
+console.group("Group 1")
+console.log("Message One")
+console.log("Message Two")
+console.group("child group")
+console.log("Message One")
+console.log("Message Two")
+console.group("Grans group")
+console.log("Message One")
+console.log("Message Two")
+console.groupEnd()
+console.groupEnd()
+console.groupEnd()
+console.group("Group 2")
+console.log("Message One")
+console.log("Message Two")
+console.groupEnd()
+console.groupEnd()
+
+//? Assignment-05
+console.table(["Elzero","Ahmed","Sameh","Gamal","Aya"])
+// console.table("Ahmed")
+// console.table("Sameh")
+// console.table("Gamal")
+// console.table("Aya")
+
+//? Assignment-06
