8e3eb986e5fd39a1eb99fe8c18826cc8e0aa357e
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-12-09 20:32:05 +0200

    added js chapter to with assignment

---
+++ JS/02- Data Types And Variables/Assignment/main.js
@@ -0,0 +1,25 @@
+//* Task one
+let numberOne = 10;
+let numberTwo = 20;
+console.log(numberOne + "" + numberTwo) //~ one
+console.log(typeof (numberOne + "" + numberTwo)) //~ two
+console.log(`${numberOne}${numberTwo}`) //~ three
+console.log(typeof(`${numberOne}${numberTwo}`)) //! four 
+console.log(numberOne+"\n"+numberTwo) //~ five
+console.log(`${numberOne}
+${numberTwo}`)
+
+
+//* Task Two
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+//* Task Three
+
+console.log("`I'n\n\\\\\nLove \\\\ \"\"\" '''\n++ with ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``")
+
+//* Task Four
+
+let a = 21;
+let b = 20;
+console.log(`// _${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`)
