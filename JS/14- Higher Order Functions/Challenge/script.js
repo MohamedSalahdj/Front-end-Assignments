6276c11bd5d67318aaa9bdf86c7e593147048b6b
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-08-05 11:40:55 +0300

    addded chapter-01 challenge

--- JS/14- Higher Order Functions/Challenge/script.js
+++ JS/14- Higher Order Functions/Challenge/script.js
@@ -19,6 +19,12 @@
 
 let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
 
-let solution = '?????';
+let solution = myString.split("").filter((ele) => {
+  return isNaN(ele) && ele !==','
+}).map((ele)=> {
+  return ele === '_' ? " " : ele
+}).reduce((acc, current) => {
+  return acc + current
+}).slice(true, -isNaN(myString))
 
 console.log(solution); // Elzero Web School
